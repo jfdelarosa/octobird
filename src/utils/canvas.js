b3437a30912edd106f0ae0568b3ed54c3e17aae0
Fernando de la Rosa <fernando.delarosa@alegra.com>
2021-09-11 01:47:19 -0500

    Se genera la tabla de contribuciones con datos aleatorios

--- src/utils/canvas.js
+++ src/utils/canvas.js
@@ -1,4 +1,15 @@
 const { createCanvas } = require("canvas");
+const SQUARE_WIDTH = 20;
+const WEEKS = 53;
+const PADDING = 2;
+
+function createSquare(ctx, x, y) {
+  ctx.beginPath();
+  ctx.fillStyle = "#1d9bf0";
+  ctx.globalAlpha = Math.random();
+  ctx.rect(x, y, SQUARE_WIDTH, SQUARE_WIDTH);
+  ctx.fill();
+}
 
 function createImage(user) {
   const width = 1500;
@@ -7,13 +18,23 @@ function createImage(user) {
   const canvas = createCanvas(width, height);
   const ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = "#000";
+  ctx.fillStyle = "#fff";
   ctx.fillRect(0, 0, width, height);
 
   ctx.textAlign = "right";
-  ctx.fillStyle = "#fff";
-  ctx.font = "bold 18px Menlo";
+  ctx.fillStyle = "#0f1419";
+  ctx.font = "18px Menlo";
   ctx.fillText(`@${user}`, width - 100, height - 100);
+
+  for (let i = WEEKS; i > 0; i--) {
+    for (let j = 7; j > 0; j--) {
+      createSquare(
+        ctx,
+        width - 100 - i * (SQUARE_WIDTH + PADDING),
+        height - 120 - j * (SQUARE_WIDTH + PADDING)
+      );
+    }
+  }
 
   return canvas.toBuffer("image/png");
 }
